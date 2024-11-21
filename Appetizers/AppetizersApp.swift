fb1d93f17c9b632277827ac4a6cd03fd388a458e
ahmetbostanciklioglu <ahmetbostancikli@gmail.com>
2024-11-21 12:24:05 +0300

    Initial Commit

---
+++ Appetizers/AppetizersApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppetizersApp.swift
+//  Appetizers
+//
+//  Created by Ahmet Bostancıklıoğlu on 21.11.2024.
+//
+
+import SwiftUI
+
+@main
+struct AppetizersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
